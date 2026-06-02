809d71cb841736ac5b75f02e3430468e83eff2ac
Eduardo Demetrio <demetrioe153@gmail.com>
2026-06-02 07:47:17 -0300

    new file:   __pycache__/defs.cpython-314.pyc new file:   defs.py new file:   main.py

---
+++ defs.py
@@ -0,0 +1,130 @@
+book_list = []
+
+def line():
+    print("\n---------- // ----------\n")
+
+def create_new_book():
+
+    line()
+
+    book = {
+        "ID": int(input("ID: ")),
+        "Title": input("Title: ").title(),
+        "Author": input("Author: ").title(),
+        "Year": int(input("Year: ")),
+        "Available": True
+    }
+
+    book_list.append(book)
+
+    print("\nBook successfully created!")
+
+    line()
+
+def show_book(book):
+
+    print(f"""
+ID: {book["ID"]}
+Title: {book["Title"]}
+Author: {book["Author"]}
+Year: {book["Year"]}
+Available: {book["Available"]}
+""")
+
+def show_book_list():
+
+    line()
+
+    if len(book_list) > 0:
+
+        for book in book_list:
+            show_book(book)
+            line()
+
+    else:
+        print("Didn't have any registered book.")
+        line()
+
+def search_title():
+
+    title = input("Book Title: ")
+
+    line()
+
+    found = False
+
+    for book in book_list:
+
+        if title.lower() in book["Title"].lower():
+
+            show_book(book)
+
+            found = True
+
+            line()
+
+    if found == False:
+        print("Book not found.")
+        line()
+
+def remove_by_id(book_id):
+
+    line()
+
+    found = False
+
+    for i, book in enumerate(book_list):
+
+        if book_id == book["ID"]:
+
+            print(book["Title"], "was successfully removed!")
+
+            book_list.pop(i)
+
+            found = True
+
+            line()
+
+            break
+
+    if found == False:
+        print("ID not found.")
+        line()
+
+def borrow_book(book_id):
+
+    for i, book in enumerate(book_list):
+
+        if book_id == book["ID"]:
+
+            if book["Available"] == True:
+                
+                line()
+
+                print(f"You sucessfully borrowed {book["Title"]} book!")
+                book["Available"] = False
+                
+            elif book["Available"] == False:
+
+                line()
+
+                print(f"{book["Title"]} cannot be borrowed!")
+
+def return_book(book_id):
+
+    for i, book in enumerate(book_list):
+
+        if book_id == book["ID"]:
+
+            if book["Available"] == False:
+                
+                line()
+
+                print(f"{book["Title"]} has returned to library!")
+                book["Available"] = True
+
+            elif book["Available"] == True:
+                
+                line()
+
+                print(f"{book["Title"]} is already on library!")
---
+++ main.py
@@ -0,0 +1,68 @@
+from defs import (
+    create_new_book,
+    show_book_list,
+    search_title,
+    remove_by_id,
+    borrow_book,
+    return_book,
+    line
+)
+
+program = True
+
+while program:
+
+    choose = input("""1 - Create New Book
+2 - Book List
+3 - Title Search
+4 - Remove by ID
+5 - Borrow Book
+6 - Return Book
+L - Finish Program
+
+Choose: """).upper()
+
+    if choose == "1":
+
+        create_new_book()
+
+    elif choose == "2":
+
+        show_book_list()
+
+    elif choose == "3":
+
+        search_title()
+
+    elif choose == "4":
+        
+        user_id = int(input("Book ID: "))
+        remove_by_id(user_id)
+    
+    elif choose == "5":
+
+        user_id = int(input("Book ID: "))
+        borrow_book(user_id)
+
+    elif choose == "6":
+
+        user_id = int(input("Book ID: "))
+        return_book(user_id)
+
+    elif choose in ["L", "LE", "LEA", "LEAV", "LEAVE"]:
+
+        line()
+
+        program = False
+
+        print("Finishing program!")
+
+        line()
+
+    else:
+
+        line()
+
+        print("Error! Choose one of the choices!")
+
+        line()
